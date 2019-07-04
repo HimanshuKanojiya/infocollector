ddc94fbbb5a835d16169b0798428f1da1e915755
Himanshu Kanojiya <33418139+HimanshuKanojiya@users.noreply.github.com>
2019-07-04 20:00:19 +0530

    Add files via upload

--- info_extractor_raw.py
+++ info_extractor_raw.py
@@ -21,6 +21,8 @@ def re_strings(content):
     strs_data = ["[a-zA-Z0-9.-]+@[a-zA-Z.-]+com","[\d]{2,3}\s[\d]{2,4}.\s[\d]{3,4}"]
     sitemaps = ["[a-zA-Z0-9.-/]+[a-z0-9._-]+xml","[a-zA-Z0-9.-/]+[a-z0-9.-]+html","[a-zA-Z0-9.-/]+[a-z0-9/-]+"]
     robos = ["[a-zA-Z.-_?]+[0-9]+","[a-zA-Z.-/_]+xml"]
+    server = {"https://ipinfo.io/":"?token=fb0e06e163165f"}
+    sec_app = ["sitemap.xml"]
 
     if str(content) == str("strs_data"):
         return strs_data
@@ -30,4 +32,13 @@ def re_strings(content):
 
     elif str(content) == str("robos"):
         return robos
+
+    elif str(content) == str("server"):
+        return server
+
+    elif str(content) == str("sec_app"):
+        return sec_app
+
+def menu(to_do):
+    print("Available settings\n")
     
